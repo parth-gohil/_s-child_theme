668c8317554ed01f113ab8f9974f0a411ec8e531
Parth Gohil <parth.gohil@gmail.com>
2018-05-31 00:53:55 +0530

    Add child theme support

--- functions.php
+++ functions.php
@@ -6,44 +6,44 @@
  * and open the template in the editor.
  */
 
+if ( ! function_exists('_s_child_theme_task_init') ) {
+
 // Register Custom Post Type
-function custom_post_type() {
+function _s_child_theme_task_init() {
 
 	$labels = array(
 		'name'                  => _x( 'Tasks', 'Post Type General Name', 'text_domain' ),
 		'singular_name'         => _x( 'Task', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'             => __( 'Post Types', 'text_domain' ),
-		'name_admin_bar'        => __( 'Post Type', 'text_domain' ),
-		'archives'              => __( 'Item Archives', 'text_domain' ),
-		'attributes'            => __( 'Item Attributes', 'text_domain' ),
-		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
-		'all_items'             => __( 'All Items', 'text_domain' ),
-		'add_new_item'          => __( 'Add New Item', 'text_domain' ),
+		'menu_name'             => __( 'Task Types', 'text_domain' ),
+		'name_admin_bar'        => __( 'Task Type', 'text_domain' ),
+		'archives'              => __( 'Task Archives', 'text_domain' ),
+		'attributes'            => __( 'Task Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Task:', 'text_domain' ),
+		'all_items'             => __( 'All Tasks', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Task', 'text_domain' ),
 		'add_new'               => __( 'Add New', 'text_domain' ),
-		'new_item'              => __( 'New Item', 'text_domain' ),
-		'edit_item'             => __( 'Edit Item', 'text_domain' ),
-		'update_item'           => __( 'Update Item', 'text_domain' ),
-		'view_item'             => __( 'View Item', 'text_domain' ),
-		'view_items'            => __( 'View Items', 'text_domain' ),
-		'search_items'          => __( 'Search Item', 'text_domain' ),
+		'new_item'              => __( 'New Task', 'text_domain' ),
+		'edit_item'             => __( 'Edit Task', 'text_domain' ),
+		'update_item'           => __( 'Update Task', 'text_domain' ),
+		'view_item'             => __( 'View Task', 'text_domain' ),
+		'view_items'            => __( 'View Task', 'text_domain' ),
+		'search_items'          => __( 'Search Task', 'text_domain' ),
 		'not_found'             => __( 'Not found', 'text_domain' ),
 		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
 		'featured_image'        => __( 'Featured Image', 'text_domain' ),
 		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
 		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
 		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
-		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
-		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
-		'items_list'            => __( 'Items list', 'text_domain' ),
-		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
-		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into Task', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Task', 'text_domain' ),
+		'items_list'            => __( 'Tasks list', 'text_domain' ),
+		'items_list_navigation' => __( 'Tasks list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items Task', 'text_domain' ),
 	);
 	$args = array(
 		'label'                 => __( 'Task', 'text_domain' ),
-		'description'           => __( 'This is a single task', 'text_domain' ),
 		'labels'                => $labels,
 		'supports'              => array( 'title', 'editor' ),
-		'taxonomies'            => array( 'category', 'post_tag' ),
 		'hierarchical'          => true,
 		'public'                => true,
 		'show_ui'               => true,
@@ -60,4 +60,6 @@ function custom_post_type() {
 	register_post_type( 'task', $args );
 
 }
-add_action( 'init', 'custom_post_type', 0 );
+add_action( 'init', '_s_child_theme_task_init', 0 );
+
+}
