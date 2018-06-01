c621ea79895ea6c7021a605bd62e0171df38d53b
Parth Gohil <parth.gohil@gmail.com>
2018-06-01 09:01:56 +0530

    add metabox to functions.php

--- functions.php
+++ functions.php
@@ -82,3 +82,29 @@ if (!function_exists('_s_child_theme_task_register')) {
 
 	add_action('init', '_s_child_theme_task_register', 0);
 }
+/**
+ * Register meta box(es).
+ */
+function wpdocs_register_meta_boxes() {
+    add_meta_box( 'meta-box-id', __( 'My Meta Box', 'textdomain' ), 'wpdocs_my_display_callback', 'post' );
+}
+add_action( 'add_meta_boxes', 'wpdocs_register_meta_boxes' );
+
+/**
+ * Meta box display callback.
+ *
+ * @param WP_Post $post Current post object.
+ */
+function wpdocs_my_display_callback( $post ) {
+    // Display code/markup goes here. Don't forget to include nonces!
+}
+
+/**
+ * Save meta box content.
+ *
+ * @param int $post_id Post ID
+ */
+function wpdocs_save_meta_box( $post_id ) {
+    // Save logic goes here. Don't forget to include nonce checks!
+}
+add_action( 'save_post', 'wpdocs_save_meta_box' );
